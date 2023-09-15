b1f9e831a6a8af5e2cc2bcecc42095efb03dde41
ShrutikaKinariwala <145088967+ShrutikaKinariwala@users.noreply.github.com>
2023-09-15 16:02:18 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import Streamlit
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
