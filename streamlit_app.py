800e97052012f8084e428c79f010e97137a8bb72
ShrutikaKinariwala <145088967+ShrutikaKinariwala@users.noreply.github.com>
2023-09-15 16:17:31 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 
 import streamlit
 
+streamlit.title('My Parents NEw Healthy Diner')
 
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
