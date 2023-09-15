6cfa49a40593e829f95dd66f973dcc38094d7df7
ShrutikaKinariwala <145088967+ShrutikaKinariwala@users.noreply.github.com>
2023-09-15 15:39:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.title('Breakfast Menu')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
 
 streamlit.title('Kale, Spinach & Rocket Smoothie')
+
+streamlit.title('Hard-Boiled Free-Range Egg')
